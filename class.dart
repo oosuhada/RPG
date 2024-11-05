e80684890af098a01c0b2011a66d387d4e5bc7ec
oosuhada <woosu91@gmail.com>
2024-11-05 17:38:49 +0900

    feat: Class generation

---
+++ class.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+import 'dart:math';
+
+// 캐릭터 클래스 정의
+class Character {
+  String name;
+  int health;
+  int attack;
+  int defense;
+
+  Character(this.name, this.health, this.attack, this.defense);
+}
